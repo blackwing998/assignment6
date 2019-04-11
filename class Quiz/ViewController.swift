4523d3d9da3a7e624f457bf1b500e8b511583fe5
james cotter <james.cotter@smail.astate,edu>
2019-04-11 14:05:38 -0500

    Initial Commit

---
+++ class Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  class Quiz
+//
+//  Created by James.Cotter on 4/11/19.
+//  Copyright © 2019 James Cotter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
